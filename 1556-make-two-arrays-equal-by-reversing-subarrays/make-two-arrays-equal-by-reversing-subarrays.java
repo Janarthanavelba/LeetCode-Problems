40b9c2c26a929a1e2ada5378cc7ec2ed4188d42c
Janarthanavel K <janaabd1718@gmail.com>
2025-07-16 20:11:07 +0530

    Time: 5 ms (32.28%) | Memory: 44.3 MB (65.76%) - LeetSync

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+
+        if(target.length!=arr.length) return false;
+
+        Arrays.sort(target);
+        Arrays.sort(arr);
+
+        if(Arrays.equals(target,arr)) return true;
+        else
+        return false;
+    }
+}
